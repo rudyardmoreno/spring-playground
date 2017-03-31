7d01f752d6eceb424b2fc1ed0356bc7d99946873
rudyardmoreno <rudyard@morenojasso.com>
2017-03-30 19:34:09 -0600

    Demo-app

---
+++ demo-app/src/main/java/com/example/HelloController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+/**
+ * Created by trainer18 on 3/30/17.
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello from Spring!...";
+    }
+}
