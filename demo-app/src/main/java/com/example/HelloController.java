d084282cdbf2666761c628d8c22615fa8b48c782
rudyardmoreno <rudyard@morenojasso.com>
2017-03-30 19:58:55 -0600

    x

--- demo-app/src/main/java/com/example/HelloController.java
+++ demo-app/src/main/java/com/example/HelloController.java
@@ -13,6 +13,6 @@ import java.sql.Date;
 public class HelloController {
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello from Spring!!!.xxxx";
+        return "Hello from Spring!!!.yyyyy";
     }
 }
