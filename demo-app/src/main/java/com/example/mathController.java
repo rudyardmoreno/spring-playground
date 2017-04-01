947977833db554bf19167ce1877ed55dd7cf2b55
rudyardmoreno <rudyard@morenojasso.com>
2017-04-01 06:20:15 -0600

    upd

--- demo-app/src/main/java/com/example/mathController.java
+++ demo-app/src/main/java/com/example/mathController.java
@@ -4,9 +4,10 @@ package com.example;
  * Created by trainer18 on 3/30/17.
  */
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/math")
@@ -15,4 +16,76 @@ public class mathController {
     public String helloWorld() {
         return "3.141592653589793";
     }
+
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam(value = "operation", defaultValue = "add") String operation,
+                            @RequestParam int x,
+                            @RequestParam int y) {
+        int result=0;
+        String txtResult;
+        String operationSign="";
+        switch (operation) {
+            case "add":
+                result=x+y;
+                operationSign="+";
+                break;
+            case "subtract":
+                result=x-y;
+                operationSign="-";
+                break;
+            case "multiply":
+                result=x*y;
+                operationSign="*";
+                break;
+            case "divide":
+                if (y!=0) {
+                    result=x/y;
+                    operationSign="/";
+                } else {
+                    operationSign="DivByZero";
+                }
+                break;
+            default:
+                operationSign="?";
+
+        }
+        switch (operationSign) {
+            case "DivByZero":
+                txtResult=String.format("%d / %d = NA (Div by Zero)",x,y);
+                break;
+            case "?":
+                txtResult=String.format("Ivalid operation (%s) for %d and %d",operation,x,y);
+                break;
+            default:
+                txtResult=String.format("%d %s %d = %d",x,operationSign,y,result);
+        }
+        return txtResult;
+    }
+
+    @PostMapping("/sum")
+    public String sum(@RequestParam MultiValueMap<String, String> querystring) {
+        //Get all map keys
+        Set<String> keys = querystring.keySet();
+        int result=0;
+        String txtResult="";
+        int count=0;
+
+        for (String key : keys) {
+            System.out.println("Key = " + key);
+            System.out.println("Values = " + querystring.get(key) + "n");
+            for (String n : querystring.get(key)) {
+                try {
+                    result += Integer.parseInt(n);
+                    if (count != 0) txtResult += " + ";
+                    txtResult += n;
+                    count += 1;
+                } catch (Exception e) {
+                    //Nothing
+                }
+            }
+        }
+        txtResult = String.format("%s = %d",txtResult,result);
+        return  txtResult;
+    }
+
 }
--- demo-app/src/test/java/com/example/mathControllerTest.java
+++ demo-app/src/test/java/com/example/mathControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,5 +26,54 @@ public class mathControllerTest {
         this.mvc.perform(get("/math/pi").accept(MediaType.TEXT_PLAIN))
                 .andExpect(status().isOk())
                 .andExpect(content().string("3.141592653589793"));
+    }
+
+    @Test
+    public void testCalculateNoOperation() throws Exception{
+        this.mvc.perform(get("/math/calculate?x=30&y=5").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 + 5 = 35"));
+    }
+
+    @Test
+    public void testCalculateAddOperation() throws Exception{
+        this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 6 = 10"));
+    }
+
+    @Test
+    public void testCalculateMultiplyOperation() throws Exception{
+        this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 * 6 = 24"));
+    }
+
+    @Test
+    public void testCalculateSubtractOperation() throws Exception{
+        this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 - 6 = -2"));
+    }
+
+    @Test
+    public void testCalculateDivideOperation() throws Exception{
+        this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=5").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 / 5 = 6"));
+    }
+
+    @Test
+    public void testCalculateDivideByZeroOperation() throws Exception{
+        this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=0").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 / 0 = NA (Div by Zero)"));
+    }
+
+    @Test
+    public void testSum() throws Exception{
+        this.mvc.perform(post("/math/sum?n=4&n=5&n=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 5 + 6 = 15"));
     }
 }
