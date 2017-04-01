cac4ad60085bee76f6b9c50e2c2e82e62637143b
rudyardmoreno <rudyard@morenojasso.com>
2017-04-01 06:44:05 -0600

    Ups

--- demo-app/src/main/java/com/example/HelloController.java
+++ demo-app/src/main/java/com/example/HelloController.java
@@ -4,12 +4,8 @@ package com.example;
  * Created by trainer18 on 3/30/17.
  */
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.Date;
 
 @RestController
 public class HelloController {
---
+++ demo-app/src/main/java/com/example/model/MathService.java
@@ -0,0 +1,7 @@
+package com.example.model;
+
+/**
+ * Created by trainer18 on 4/1/17.
+ */
+public class MathService {
+}
