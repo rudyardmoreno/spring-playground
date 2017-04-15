4164994035880457ee7264dbc34cfe8112e3c5c6
rudyardmoreno <rudyard@morenojasso.com>
2017-04-15 10:46:48 -0600

    Spring Word Count: Test Config Class

--- src/test/java/com/example/tests/StringsControllerTest3.java
+++ src/test/java/com/example/tests/StringsControllerTest3.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -36,9 +37,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc(secure=false)
-
-public class StringsControllerTest2 {
+@AutoConfigureMockMvc(secure = false)
+@TestPropertySource(properties = {
+        "wordCount.delimiter=-",
+        "wordCount.caseSensitive=true",
+        "wordCount.words.skip[0]=the\n",
+        "wordCount.words.skip[1]=an\n",
+        "wordCount.words.skip[2]=a",
+})
+public class StringsControllerTest3 {
 
     @Autowired
     MockMvc mvc;
@@ -53,16 +60,18 @@ public class StringsControllerTest2 {
     String request;
 
     @Before
-    public void test1Setup(){
-        request ="To The MOON to the moon to the moon";
-        response.put("to",3);
-        //response.put("the",3);
-        response.put("moon",3);
+    public void test1Setup() {
+        request = "To-The-MOON-to-the-moon-to-the-moon";
+        response.put("The", 1);
+        response.put("moon", 2);
+        response.put("MOON", 1);
+        response.put("To",1);
+        response.put("to", 2);
     }
 
     @Test
     public void test1() throws Exception {
-        assertEquals(response,wordCounter.count(request));
+        assertEquals(response, wordCounter.count(request));
     }
 
     @Test
@@ -73,19 +82,19 @@ public class StringsControllerTest2 {
         wordsSkip.add("an");
         wordsSkip.add("a");
 
-        assertEquals(wordCountConfig.getDelimiter()," ");
-        assertEquals(wordCountConfig.getCaseSensitive(),false);
-        assertEquals(wordCountConfig.getWords().getSkip(),wordsSkip);
-        assertThat(wordCountConfig.getDelimiter(),equalTo(" "));
-        assertThat(wordCountConfig.getWords().getSkip(),contains("the","an","a"));
+        assertEquals(wordCountConfig.getDelimiter(), "-");
+        assertEquals(wordCountConfig.getCaseSensitive(), true);
+        assertEquals(wordCountConfig.getWords().getSkip(), wordsSkip);
+        assertThat(wordCountConfig.getDelimiter(), equalTo("-"));
+        assertThat(wordCountConfig.getWords().getSkip(), contains("the", "an", "a"));
 
     }
 
     @Test
-    public void test3() throws Exception{
+    public void test3() throws Exception {
         String words;
 
-        words="to the moon to the moon";
+        words = "to-the-moon-to-the-moon";
 
         MockHttpServletRequestBuilder request = post("/words/count")
                 .contentType(MediaType.TEXT_PLAIN)
@@ -94,7 +103,7 @@ public class StringsControllerTest2 {
         this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.to", is(2)))
-                .andExpect( jsonPath("$.the").doesNotExist())
+                .andExpect(jsonPath("$.the").doesNotExist())
                 .andExpect(jsonPath("$.moon", is(2)))
 
         ;
